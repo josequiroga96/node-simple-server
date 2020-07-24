83e7281333f614fde40176632b4b4a053f87a8c9
JoseRojas96 <jose.rojas@ing.austral.edu.ar>
2020-07-24 14:02:14 -0300

    Added timeout to request

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ var corsOptions = {
 }
 
 app.get('/', cors(corsOptions), (req, res) => res.send('Hello World!'))
-app.post('/success', cors(corsOptions), (req, res) => res.status(201).send('Success!'))
-app.post('/error', cors(corsOptions), (req, res) => res.status(400).send('Error!'))
-app.post('/conflict', cors(corsOptions), (req, res) => res.status(422).send({errors: {email: "has already been taken"}}))
+app.post('/success', cors(corsOptions), (req, res) => {
+	console.log("Sending success response!!");
+	setTimeout(() => res.status(201).send('Success!'), 2000);
+})
+app.post('/error', cors(corsOptions), (req, res) => {
+	console.log("Sending error response!!");
+	setTimeout(() => res.status(400).send('Error!'), 2000);
+})
+app.post('/conflict', cors(corsOptions), (req, res) => {
+	console.log("Sending conflict response!!");
+	setTimeout(() => res.status(422).send({errors: {email: "has already been taken"}}), 2000);
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
